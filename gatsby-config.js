529c39452b975c1545a8a7538a7bd21d8e910fa0
shingkid <seah.jane@gmail.com>
2023-10-01 20:09:34 +0800

    Update skills and hobbies

--- gatsby-config.js
+++ gatsby-config.js
@@ -133,7 +133,7 @@ module.exports = {
     skills: [
       {
         name: 'Languages & Frameworks',
-        description: 'Java, Spring, Python, Django, Flask, Clojure, Ruby',
+        description: 'Java, Spring, Python,, PyTorch, Django, Flask, Clojure, Ruby',
       },
       {
         name: 'Databases',
@@ -149,7 +149,7 @@ module.exports = {
       {
         name: 'Water Sports',
         description:
-          'Snorkelling 🤿, surfing 🏄‍♀️, diving, and ...eating fish?',
+          'Snorkelling/diving 🤿, surfing 🏄‍♀️, and ...eating fish 😋',
       },
       {
         name: 'Roguelike Videogames',
